6d9915a451494dfd349deca8fa28dcbeba177e23
littletof <szalay.kristof.sopron@gmail.com>
2020-12-29 14:55:06 +0100

    update deps

--- deps.ts
+++ deps.ts
@@ -2,4 +2,4 @@ export { Select } from "https://deno.land/x/cliffy@v0.16.0/prompt/select.ts";
 export type  { SelectValueOptions } from "https://deno.land/x/cliffy@v0.16.0/prompt/select.ts";
 
 export { Input } from "https://deno.land/x/cliffy@v0.16.0/prompt/input.ts";
-export  * as colors from 'https://deno.land/std@0.74.0/fmt/colors.ts';
+export  * as colors from 'https://deno.land/std@0.82.0/fmt/colors.ts';
