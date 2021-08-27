46d750d7d421b1166beb62f7c3845ec89d6819f1
littletof <szalay.kristof.sopron@gmail.com>
2021-08-27 12:44:11 +0200

    tests: Add basic in-out tester

---
+++ tests/integration/test.ts
@@ -0,0 +1,145 @@
+import {
+    assertEquals,
+    dirname,
+    expandGlob,
+    WalkEntry,
+    iter
+} from "../test_deps.ts";
+
+import { upInCL } from "../../utils.ts";
+  
+  const baseDir = `${dirname(import.meta.url).replace("file://", "")}`;
+  
+  for await (const file: WalkEntry of expandGlob(`${baseDir}/fixtures/*.in`)) {
+    if (file.isFile) {
+      const name = file.name.replace(/_/g, " ").replace(".in", "");
+      Deno.test({
+        name: `kopo - integration - ${name}`,
+        async fn() {
+          const output: string = await runPrompt(file);
+          const expectedOutput: string = await getExpectedOutput(file.path);
+          // console.log(JSON.stringify(cleanOutput(output).split('##CLSX##')));
+          assertEquals(
+            cleanOutput(output)
+              .replace("#END#", "")
+              .split('##CLSX##')
+              .at(-1),
+            expectedOutput
+              .replace(/\r\n/g, "\n")
+
+          );
+        }
+      });
+    }
+  }
+
+  function cleanOutput(output: string) {
+    return output
+            .replaceAll(/\r\n/g, "\n")
+            .replaceAll('\x1Bc', "##CLS-KOPO##") // used by Kopo
+            .replaceAll('\u001b[0J', '##CLSX##') // cls
+            .replaceAll(upInCL(1) + " ".repeat(70) + upInCL(1)+'\n', '##cl-up2##')
+            .replaceAll('\u001b[?25l', ''/* , '#l#' */) // hidecursor
+            .replaceAll('\u001b[?25h', ''/* , '#h#' */) // showcursor
+            //.replaceAll('\u001b[6A', ''/* , '#˘#' */) // cursor up 6
+            .replaceAll('\u001b[G', ''/* , '#g#' */) // cursor stuff?
+            // .replaceAll('\u001b[17G', '') // cursor horizontal absolute. 
+            .replace(/\u001b\[[0-9]+m/g, '') // text formatting
+            .replaceAll('\b', '×')
+            .replaceAll(/\u001b\[(\d+)G/g, '##cha$1##') // cursor horizontal absolute. 1 = first char of the line
+            .replaceAll(/\u001b\[(\d+)A/g,"##up$1##") // cursor up \d
+            .replace(/##CLSX##$/g, '#END#') // clsx at the end
+            // .replaceAll(/\n[^×\n]*×+/g, '\n') // clear before backspace
+  } 
+  
+  async function getExpectedOutput(path: string) {
+    const osOutputPath = path.replace(/\.in$/, `.${Deno.build.os}.out`);
+    try {
+      return await Deno.readTextFile(osOutputPath);
+    } catch (_) {
+      const outputPath = path.replace(/\.in$/, ".out");
+      return await Deno.readTextFile(outputPath);
+    }
+  }
+  
+  function getCmdFlagsForFile(file: WalkEntry): string[] {
+    /* if (file.name === "input_no_location_flag.ts") {
+      return [
+        "--unstable",
+        "--allow-all",
+      ];
+    }
+    return [
+      "--unstable",
+      "--allow-all",
+      "--location",
+      "https://kopo.mod.land",
+    ]; */
+    return [
+      "--allow-net", // TODO minimal
+      "--allow-read", // TODO min glob
+      "--unstable",
+    ]
+  }
+  
+  async function runPrompt(file: WalkEntry): Promise<string> {
+    const inputPath: string = file.path.replace(/\.ts$/, ".in");
+    const inputFile = await Deno.open(inputPath);
+    const inputText = await Deno.readTextFile(inputPath);
+    const flags = getCmdFlagsForFile(file);
+    const process = Deno.run({
+      stdin: "piped",
+      stdout: "piped",
+      cmd: [
+        "deno",
+        "run",
+        ...flags,
+        'index.ts',
+        "-r", new URL('../test_registry.ts', import.meta.url).href, // TODO not always needed, eg. registrySelect
+      ],
+      env: {
+        NO_COLOR: "true",
+      },clearEnv:true
+    });   
+
+
+    let result = '';
+    process.stdin.write(new TextEncoder().encode(inputText)).then(async _ => {/* await delay(100);  */await process.stdin.write(new TextEncoder().encode('\u0003'));});
+    for await (let a of iter(process.stdout)) {
+      result += new TextDecoder().decode(a);
+    }
+
+    // console.log('itsa me', cleanOutput(result));
+
+    /* const [output, bytesCopied] = await Promise.all([
+      process.output(),
+      // copy(inputFile, process.stdin),
+      new Promise<number>(async (res, rej) => { 
+        // const bytes = await copy(inputFile, process.stdin);
+        await delay(1000);
+        await process.stdin.write(new TextEncoder().encode('\u0003'));
+        process.stdin.close(); 
+        process.close(); 
+        // res(bytes);  
+        res(1);  
+      }),
+      // new Promise<number>(async (res, rej) => { const bytes = await copy(inputFile, process.stdin); setTimeout(async () => {await process.stdin.write(new TextEncoder().encode('\u0003')); res(bytes)}, 1000);  }),
+      // new Promise<number>(async (res, rej) => { const bytes = await copy(inputFile, process.stdin); await process.stdin.write(new TextEncoder().encode('\u0003')); res(bytes)  })
+      // new Promise<number>((res, rej) => { copy(inputFile, process.stdin).then(bytes => {queueMicrotask(()=> {process.stdin.write(new TextEncoder().encode('\u0003')); res(bytes);})})}),
+    ]); */
+
+    // console.log('OUT', new TextDecoder().decode(await process.output()).split(''));
+
+
+
+    inputFile.close();
+    process.stdin.close();
+    process.stdout.close();
+    process.close();
+
+  
+    // assert(bytesCopied > 0, "No bytes copied");
+  
+    // return new TextDecoder().decode(/* output */);
+    return cleanOutput(result);
+  }
---
+++ tests/test_registry.ts
@@ -0,0 +1,80 @@
+import { Registry } from "../registries/registry.ts";
+
+export class TestRegistry extends Registry {
+    static key = 'test_registry';
+
+    getWellKnownPath() {
+        return "test_wellknown";
+    }
+
+    getRegistryInfo() {
+        return {
+            key: TestRegistry.key,
+            name: 'Test registry',
+            icon: '🐛',
+            url: 'https://test.land',
+            description: '`TEST ADDON REGISTRY`'
+        }
+    }
+
+    async getAllModuleNames() {
+        return ['test1', 'test2', 'test3', 'test4'];
+    }
+
+    async getVersionsOfModule(moduleName: string) {
+        return ['v1', 'v2', 'v3']
+    }
+
+    async getModulesList(query?: string, page: number=1, pageSize: number = 20) {
+        return {
+            modules: [
+                { name: 'test1', description: 'test1 description', starCount: 10, owner: 'me' },
+                { name: 'test2', description: 'test2 description', starCount: 20, owner: 'me' },
+                { name: 'test3', description: 'test3 description', starCount: 30, owner: 'me' },
+                { name: 'test4', description: 'test4 description', starCount: 40, owner: 'me' },
+            ],  
+            page: 1,
+            pageSize: 1,
+            totalModules: 4,
+            totalPages: 1,
+        }
+    }
+
+    async getModuleInfo(moduleName: string, version?: string) {
+        if(!['test1','test2','test3','test4'].includes(moduleName)) {
+            return undefined;
+        }
+
+        return {
+            origin: TestRegistry.key,
+            info: {
+                name: moduleName,
+                description: moduleName + " descrition",
+                start_count: 5,
+                versions: ['v1', 'v2', 'v3'],
+                latestVersion: 'v3',
+
+                repository: 'https://test.repo',
+                moduleRoute: 'https://x/test.repo',
+                // importRoute: 'testt',
+                flags: {
+                    required: [{flag: "--unstable", description: "needed"}],
+                    optional: [{flag: "--location", description: "optional"}],
+                }
+            },
+            invalidVersion: false,
+            currentVersion: 'testt',
+            uploadedAt: new Date(),
+            readmePath: 'testt',
+            readmeText: 'testt',
+        }
+    }
+
+    private getRepositoryPath(upload_options?: {type: string, repository: string, ref: string}, version?: string): string | undefined {
+        return 'none';
+    }
+}
+
+export function getAddonRegistry() {
+    return new TestRegistry();
+}
